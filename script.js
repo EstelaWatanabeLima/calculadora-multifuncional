fd04998d2e108c5c69793d9f8e2d12a9316af3a7
EstelaWatanabeLima <estelawatanabelima2009@gmail.com>
2026-09-18 10:50:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,10 @@ function limpar() {
     document.getElementById("numero2").value = "";
     document.getElementById("resultado").innerHTML = "Resultado:";
 }
+
+
+function limpar() {
+    document.getElementById("numero1").value = "";
+    document.getElementById("numero2").value = "";
+    document.getElementById("resultado").innerHTML = "Resultado:";
+}
